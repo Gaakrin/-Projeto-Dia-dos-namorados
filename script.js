824577eae2749f9bd39eadf7436f8d907300f9ea
Gaakrin <51807507+Gaakrin@users.noreply.github.com>
2024-07-26 22:38:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function fadeImage1(){
+    document.getElementById("Imagem1").style.animation = "sumir 0.5s linear"
+    document.getElementById("texto1").style.animation = "aparecer 0.5s linear"
+    setTimeout(() => document.getElementById("Imagem1").style.opacity = "0%", 500);
+    setTimeout(() => document.getElementById("texto1").style.opacity = "100%", 500);
+}
+function fadeImage2(){
+    document.getElementById("Imagem2").style.animation = "sumir 0.5s linear"
+    document.getElementById("texto2").style.animation = "aparecer 0.5s linear"
+    setTimeout(() => document.getElementById("Imagem2").style.opacity = "0%", 500);
+    setTimeout(() => document.getElementById("texto2").style.opacity = "100%", 500);
+}
+function fadeImage3(){
+    document.getElementById("Imagem3").style.animation = "sumir 0.5s linear"
+    document.getElementById("texto3").style.animation = "aparecer 0.5s linear"
+    setTimeout(() => document.getElementById("Imagem3").style.opacity = "0%", 500);
+    setTimeout(() => document.getElementById("texto3").style.opacity = "100%", 500);
+}
+function fadeImage4(){
+    document.getElementById("Imagem4").style.animation = "sumir 0.5s linear"
+    document.getElementById("texto4").style.animation = "aparecer 0.5s linear"
+    setTimeout(() => document.getElementById("Imagem4").style.opacity = "0%", 500);
+    setTimeout(() => document.getElementById("texto4").style.opacity = "100%", 500);
+}
+function fadeImage5(){
+    document.getElementById("Imagem5").style.animation = "sumir 0.5s linear"
+    document.getElementById("texto5").style.animation = "aparecer 0.5s linear"
+    setTimeout(() => document.getElementById("Imagem5").style.opacity = "0%", 500);
+    setTimeout(() => document.getElementById("texto5").style.opacity = "100%", 500);
+}
